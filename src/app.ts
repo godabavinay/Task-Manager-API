3dc24879eb5d22c4e185452db5ff46b17ca28401
Vinay Godaba <godabavinay@gmail.com>
2025-05-24 19:05:37 +0530

    (test): Added Integration tests for tasks route

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import tasksRoutes from './routes/tasks';
 const app = express();
 
 app.use(express.json());
-app.use('/', (_, res) => { res.status(200).json({ message: 'API is up and running' }) })
 app.use('/tasks', tasksRoutes);
+app.use('/', (_, res) => { res.status(200).json({ message: 'API is up and running' }) });
 
 export default app;
