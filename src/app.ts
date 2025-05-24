b3ba73ac3dbdf7edc34a319c0d4cd4d4744bbff8
Vinay Godaba <godabavinay@gmail.com>
2025-05-24 17:58:22 +0530

    (init): Initialize integration tests

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import tasksRoutes from './routes/tasks';
 const app = express();
 
 app.use(express.json());
-app.use('/', (_, res) => { res.json({ message: 'API is up and running' }) })
+app.use('/', (_, res) => { res.status(200).json({ message: 'API is up and running' }) })
 app.use('/tasks', tasksRoutes);
 
 export default app;
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
     test: {
         globals: true,
         environment: 'node',
-        include: ['src/**/*.test.ts'],
+        include: ['src/**/*.test.ts', 'tests/integration/**/*.test.ts'],
         coverage: {
             reporter: ['text', 'json', 'html'],
         }
