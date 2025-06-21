fdba66ad3a5926739dfcc7ae4206fa38438fd11f
Vinay Godaba <godabavinay@gmail.com>
2025-06-21 17:22:31 +0530

    (test): Load tests for tasks API

--- tests/loadTests/src/index.ts
+++ tests/loadTests/src/index.ts
@@ -6,8 +6,10 @@ export const options = {
     duration: '10s',
 };
 
+const BASE_API_URL = 'http://localhost:3000';
+
 export default function () {
-    const res = http.get('http://localhost:3000/tasks');
+    const res = http.get(`${BASE_API_URL}/`);
     check(res, { 'status is 200': (r) => r.status === 200 });
     sleep(1);
 }
